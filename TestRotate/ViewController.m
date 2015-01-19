506f07590c347823ea0461df51f2db5e89c95785
zangqilong <zangqilong@gmail.com>
2015-01-19 23:30:39 +0800

    添加次序动画

--- TestRotate/ViewController.m
+++ TestRotate/ViewController.m
@@ -13,6 +13,10 @@
 @interface ViewController ()
 {
     UIView *redView;
+    
+    UIView *greenView;
+    
+    UIView *blueView;
 }
 
 @end
@@ -24,14 +28,28 @@
     
     redView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
     redView.backgroundColor = [UIColor redColor];
-    redView.center = self.view.center;
+    redView.center = CGPointMake(self.view.center.x-40, self.view.center.y);
     redView.layer.cornerRadius = 5;
     redView.layer.masksToBounds = YES;
     [self.view addSubview:redView];
+    
+    greenView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    greenView.backgroundColor = [UIColor greenColor];
+    greenView.center = CGPointMake(self.view.center.x+40, self.view.center.y);
+    greenView.layer.cornerRadius = 5;
+    greenView.layer.masksToBounds = YES;
+    [self.view addSubview:greenView];
+    
+    blueView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    blueView.backgroundColor = [UIColor blueColor];
+    blueView.center = CGPointMake(self.view.center.x+120, self.view.center.y);
+    blueView.layer.cornerRadius = 5;
+    blueView.layer.masksToBounds = YES;
+    [self.view addSubview:blueView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (CAKeyframeAnimation *)createRotateAnimation
+- (CAKeyframeAnimation *)createRotateAnimationWithBeginTime:(NSTimeInterval)time
 {
     CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation.z"];
     
@@ -43,6 +61,8 @@
     
     animation.removedOnCompletion = NO;
     
+    animation.beginTime = CACurrentMediaTime()+time;
+    
     animation.duration = 1;
     
     return animation;
@@ -50,12 +70,31 @@
 
 - (IBAction)startAnimation:(id)sender
 {
-    [redView.layer addAnimation:[self createRotateAnimation] forKey:@"rotate"];
+    [redView.layer addAnimation:[self createRotateAnimationWithBeginTime:0] forKey:@"rotate"];
+    [greenView.layer addAnimation:[self createRotateAnimationWithBeginTime:0.2] forKey:@"rotate2"];
+    [blueView.layer addAnimation:[self createRotateAnimationWithBeginTime:0.4] forKey:@"rotate3"];
+    
     [UIView animateWithDuration:0.5 animations:^{
         redView.center = CGPointMake(redView.center.x, redView.center.y-10);
     } completion:^(BOOL finished) {
         [UIView animateWithDuration:0.1 animations:^{
             redView.center = CGPointMake(redView.center.x, redView.center.y+10);
+        }];
+    }];
+    
+    [UIView animateWithDuration:0.5 delay:0.2 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        greenView.center = CGPointMake(greenView.center.x, greenView.center.y-10);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.1 animations:^{
+            greenView.center = CGPointMake(greenView.center.x, greenView.center.y+10);
+        }];
+    }];
+    
+    [UIView animateWithDuration:0.5 delay:0.4 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        blueView.center = CGPointMake(blueView.center.x, blueView.center.y-10);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.1 animations:^{
+            blueView.center = CGPointMake(blueView.center.x, blueView.center.y+10);
         }];
     }];
 }
