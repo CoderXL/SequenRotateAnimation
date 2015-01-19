89169d4b30c3c07970f225b20c69e0c0a6019dbf
zangqilong <zangqilong@gmail.com>
2015-01-19 22:52:56 +0800

    Initial Commit

---
+++ TestRotateTests/TestRotateTests.m
@@ -0,0 +1,40 @@
+//
+//  TestRotateTests.m
+//  TestRotateTests
+//
+//  Created by 臧其龙 on 15/1/19.
+//  Copyright (c) 2015年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TestRotateTests : XCTestCase
+
+@end
+
+@implementation TestRotateTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
